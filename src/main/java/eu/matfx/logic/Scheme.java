5d3a200a0514b286c489838fcad7b3bf602ebb10
M.Goerlich <M.Goerlich@bscgmbh.de>
2019-03-01 15:15:42 +0100

    /**/

---
+++ src/main/java/eu/matfx/logic/Scheme.java
@@ -0,0 +1,11 @@
+package eu.matfx.logic;
+
+/**
+ * Scheme is the template and representing the graphical instance
+ * <br>The scheme contains the complete definied workflow.
+ * @author m.goerlich
+ *
+ */
+public class Scheme {
+
+}
