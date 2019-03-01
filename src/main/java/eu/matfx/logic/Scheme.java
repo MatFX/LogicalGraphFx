c701e90372480cb07665739ff709982150218e55
M.Goerlich <M.Goerlich@bscgmbh.de>
2019-03-01 15:42:48 +0100

    /**/

--- src/main/java/eu/matfx/logic/Scheme.java
+++ src/main/java/eu/matfx/logic/Scheme.java
@@ -1,5 +1,11 @@
 package eu.matfx.logic;
 
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import eu.matfx.logic.data.ALogicElement;
+
 /**
  * Scheme is the template and representing the graphical instance
  * <br>The scheme contains the complete definied workflow.
@@ -7,5 +13,46 @@ package eu.matfx.logic;
  *
  */
 public class Scheme {
+	
+	public static final int START_INDEX = 0;
+	
+	/**
+	 * describes the complete workflow of an event. The first element is ever at index 0 (START_INDEX)
+	 * <br>The indices are continuously without a numerical gap.
+	 */
+	private SortedMap<Integer, ALogicElement> workflowMap = new TreeMap<Integer, ALogicElement>();
+	
+	public void addElementAtMap(ALogicElement logicElement)
+	{
+		workflowMap.put(workflowMap.size(), logicElement);
+	}
+
+	public void deleteElementMap(int index)
+	{
+		workflowMap.remove(index);
+	}
+	
+	/**
+	 * check the map; only validated maps will be produce legal results 
+	 * @return
+	 */
+	public boolean isWorkflowMapUseable()
+	{
+		if(workflowMap == null || workflowMap.size() <= 0)
+			return false;
+		
+		int index = 0;
+		for(Entry<Integer, ALogicElement> entry : workflowMap.entrySet())
+		{
+			//TODO more checks at the values
+			if(entry.getKey() != index)
+				return false;
+			
+			index++;
+		}
+		return true;
+	}
+	
+	
 
 }
---
+++ src/main/java/eu/matfx/logic/data/impl/LineConnector.java
@@ -0,0 +1,13 @@
+package eu.matfx.logic.data.impl;
+
+import eu.matfx.logic.data.ALogicElement;
+
+/**
+ * This object connect two different LogicElements. At the ui it will be represented with a line.
+ * @author m.goerlich
+ *
+ */
+public class LineConnector extends ALogicElement
+{
+
+}
